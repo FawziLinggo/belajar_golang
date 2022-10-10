078bfd8c2716fcae78dbc842fb68f5353dfc03f6
vendor25 <bobby@mitraintibersama.com>
2022-10-10 14:28:11 +0700

    "Variable"

---
+++ helloworld/number.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("angka : ", 1)
+	fmt.Println("angka : ", 3.5)
+}
---
+++ variables/var_string.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Fawzi Linggo"
+	fmt.Println(name)
+
+	name = "vanger"
+	fmt.Println(name)
+
+	var friendsname = "Budi"
+	fmt.Println(friendsname)
+
+	var age = 45
+	fmt.Println(age)
+
+	country := "America"
+	fmt.Println(country)
+
+	var (
+		firstName = "Fawzi"
+		lastName  = "Linggo"
+	)
+	fmt.Println("my name is : ", firstName, lastName)
+}
