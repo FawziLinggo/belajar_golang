f56485258a60266a41a31e4c8c9b43872f38edf8
MIB <bobby@mitraintibersama.com>
2022-10-14 13:22:43 +0700

    error interface

---
+++ interface/interface-error.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func pembagian(nilai int, pembagi int) (int, error) {
+	if pembagi == 0 {
+		return 0, errors.New("Pembagi tidak boleh 0")
+	} else {
+		return nilai / pembagi, nil
+	}
+}
+
+func main() {
+	hasilPembagian, error := pembagian(10, 0)
+
+	if error == nil {
+		fmt.Println("Hasil", hasilPembagian)
+	} else {
+		fmt.Println(error)
+	}
+
+}
